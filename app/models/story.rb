980c23eab23d5bf703360d2dfda598fa7073b52b
Caitlin Woodward <cwoodward@coroutine.com>
2011-03-30 11:24:36 -0500

    Completed voting feature

--- app/models/story.rb
+++ app/models/story.rb
@@ -23,4 +23,25 @@ class Story < ActiveRecord::Base
      link.starts_with?("http://") ? link : "http://#{link}"
    end
 
+   def upvoted_by_user?(user)
+     voted_by_user(user, "up")
+   end
+   
+   def downvoted_by_user?(user)
+     voted_by_user(user, "down")
+   end
+   
+   # -------------------------------------------------
+   #  Private methods
+   # -------------------------------------------------
+
+   def voted_by_user(user, up_or_down)
+     voted = false
+     unless user.nil?
+       user_vote = Vote.where(:story_id => self.id, :user_id => user.id).first
+       voted = true if user_vote && user_vote.up_or_down == up_or_down
+     end
+     voted
+   end
+
 end
